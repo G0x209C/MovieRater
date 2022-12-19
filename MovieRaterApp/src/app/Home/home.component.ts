7b179cf4a002f5a14881c94bf4cf99b7092dcd08
G0x209C <g@collette-online.nl>
2022-12-19 23:22:52 +0100

    :godmode: Basic angular setup, incomplete thusfar

---
+++ MovieRaterApp/src/app/Home/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'mr-page-home',
+  templateUrl: './home.component.html'
+})
+export class HomePageComponent{
+
+}
--- MovieRaterApp/src/app/app-routing.module.ts
+++ MovieRaterApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Home/home.component';
+import { Error404Component } from './Misc/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
